4dcd9fdfe4c7984ede10131650467920e0ef21ec
learn <github email address>
2020-03-11 11:35:32 -0700

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React, {Component} from 'react'
+
+export default class Keypad extends React.Component{
+    tickleMe = () => {console.log("Entering password...")}
+
+    render(){
+        return <input onKeyUp={this.tickleMe} type="password"/>
+    }
+}
+
+
